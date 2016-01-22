4b642c81f4a73ade7c804528fb98d354b789ef01
jhleong@nuc <jhleong@gmail.com>
2016-01-22 16:50:47 +0800

    implemented 3 URI

--- BarRecord.cs
+++ BarRecord.cs
@@ -2,38 +2,135 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Runtime.Serialization;using ThomsonReuters.Desktop.SDK.DataAccess.TimeSeries;
-
-namespace DailyIntervalDemo
+using System.Runtime.Serialization;
+using ThomsonReuters.Desktop.SDK.DataAccess.TimeSeries;
+using ThomsonReuters.Desktop.SDK.DataAccess.TimeSeries.Metadata;
+
+namespace DailyIntervalDemo
+
 {
         [DataContract]
         public class BarRecord
         {
 
             [DataMember]
-            public double close { get; set; }
+            public double cl { get; set; } // CLOSE
 
             [DataMember]
-            public double open { get; set; }
+            public double op { get; set; }  // OPEN
 
             [DataMember]
-            public double high { get; set; }
+            public double hi { get; set; }  // HIGH
 
             [DataMember]
-            public double low { get; set; }
+            public double lo { get; set; } // LOW
 
             [DataMember]
-            public DateTime timestamp { get; set; }
+            public double vol { get; set; } // VOLUME
+
+            [DataMember]
+            public string ts { get; set; }  // TimeStamp
 
         public BarRecord(IBarData bardata)
-            {
-                close = (double)bardata.Close;
-                open = (double)bardata.Open;
-                high = (double)bardata.High;
-                low = (double)bardata.Low;
-                timestamp = (DateTime)bardata.Timestamp;
+            {            
+                cl = (double)bardata.Close;
+                op = (double)bardata.Open;
+                hi = (double)bardata.High;
+                lo = (double)bardata.Low;
+                vol = (double)bardata.Volume; 
+                DateTime dt_ts = (DateTime)bardata.Timestamp;
+                ts = dt_ts.ToString("yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture);
             }
 
    
-    }
+        }
+
+    [DataContract]
+    public class NavRecord
+    {
+
+        [DataMember]
+        public double nav { get; set; }
+
+        [DataMember]
+        public string ts { get; set; }
+
+        public NavRecord(IData idata)
+        {
+            nav = (double)idata["VALUE"];
+            DateTime dt_ts = (DateTime)idata.Timestamp;
+            ts = dt_ts.ToString("yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+
+    }
+
+
+    [DataContract]
+    public class MetaDataRecord
+    {
+
+        [DataMember]
+        public int bond_type { get; set; }
+
+        [DataMember]
+        public string currency { get; set; }
+
+        [DataMember]
+        public string exchange { get; set; }
+
+        [DataMember]
+        public string expiry { get; set; }
+
+        [DataMember]
+        public int instrument_type { get; set; }
+
+        [DataMember]
+        public int language_id { get; set; }
+
+        [DataMember]
+        public string language_name { get; set; }
+
+        [DataMember]
+        public string long_name { get; set; }
+
+        [DataMember]
+        public int market_sector { get; set; }
+
+        [DataMember]
+        public string maturity { get; set; }
+
+        [DataMember]
+        public string notes { get; set; }
+
+        [DataMember]
+        public int rec_type { get; set; }
+
+        [DataMember]
+        public string ric_name { get; set; }
+
+        [DataMember]
+        public string timezone { get; set; }
+
+        public MetaDataRecord(CommonMetadata mdata)
+        {            
+            bond_type = mdata.BondType;
+            currency = mdata.Currency;
+            exchange = mdata.Exchange;
+            expiry = mdata.Expiry.ToString("s");
+            instrument_type = (int)mdata.InstrumentType;
+            language_id = mdata.LanguageId;
+            language_name = mdata.LanguageName;
+            long_name = mdata.LongName;
+            market_sector = (int)mdata.MarketSector;
+            maturity = mdata.Maturity.ToString("s");
+            notes = mdata.Notes;
+            rec_type = mdata.RecType;
+            ric_name = mdata.RicName;
+            timezone = mdata.TimeZone;
+        }
+
+
+    }
+
 }
--- MainControl.xaml.cs
+++ MainControl.xaml.cs
@@ -24,45 +24,54 @@ namespace DailyIntervalDemo
     using System.Threading.Tasks;
     using System.Xml;
     using System.Runtime.Serialization;
-
+    using ThomsonReuters.Desktop.SDK.DataAccess.TimeSeries.Metadata;
     [ServiceContract]
     public interface IService1
     {
         [OperationContract]
-        Task<Collection<BarRecord>> GetData(string id);
+        Task<Collection<BarRecord>> getRT_bar_data(string par_ricname, string par_interval, string par_from_date, string par_to_date);
+        [OperationContract]
+        Task<Collection<NavRecord>> getRT_nav_data(string par_ricname, string par_type, string par_interval, string par_from_date, string par_to_date);
+        [OperationContract]
+        Task<MetaDataRecord> getRT_meta_data(string par_ricname);
+
     }
 
     public class Service1 : IService1
     {
-        public ITimeSeriesDataSubscription dataSubscription { get; set; }
-       // public static ITimeSeriesDataRequest dataSubscription { get; set; }
-        public string Ric { get; set; }
-        public static ObservableCollection<IBarData> Records = new ObservableCollection<IBarData>();
-        public static Collection<BarRecord> barRecCollection = new Collection<BarRecord>();
+       // public ITimeSeriesDataSubscription dataSubscription { get; set; }
+        private static ITimeSeriesDataRequest dataRequest { get; set; }
+        private IMetadataRequest metadataRequest;
+
+        //public string Ric { get; set; }
+        //public static ObservableCollection<IBarData> Records = new ObservableCollection<IBarData>();
+        private static Collection<BarRecord> barRecCollection = new Collection<BarRecord>();
+        private static Collection<NavRecord> navRecCollection = new Collection<NavRecord>();
+
+        private MetaDataRecord metaDataRec;
+
         private static bool dataCompleted;
+        private string view_type;
         //private readonly AutoResetEvent _signal = new AutoResetEvent(false);
+
+
+
+        // rt_mkt_data/s={par_ricname}&a={par_from_month}&b={par_from_day}&c={par_from_year}&d={par_to_month}&e={par_to_day}&f={par_to_year}&g={par_interval}&h={par_type}
+
+        // rt_mkt_data/{par_ricname}/{par_type}/{par_interval}/{par_from_date}/{par_to_date}
+        //      par_type : e - equity, f - fund, b - bond
+        //      par_interval : d - daily, w - weekly, m - monthly, q - quarterly, y - yearly
+        //      par_from_date : YYYYMMDD
+        //      par_to_date : YYYYMMDD
 
         [WebInvoke(Method = "GET",
                     ResponseFormat = WebMessageFormat.Json,
-                    UriTemplate = "data/{id}")]
-        public async Task<Collection<BarRecord>> GetData(string id)
+                    UriTemplate = "rt_bar_data/{par_ricname}/{par_interval}/{par_from_date}/{par_to_date}")]
+        public async Task<Collection<BarRecord>> getRT_bar_data(string par_ricname, string par_interval, string par_from_date, string par_to_date)
         {
-            this.Ric = id;
-            dataCompleted = false;
-            Records.Clear();
-            barRecCollection.Clear();
 
-            dataSubscription = DataServices.Instance.TimeSeries.SetupDataSubscription(this.Ric)
-                                        .WithInterval(CommonInterval.Daily)
-                                        .OnDataReceived(this.OnSubscriptionDataReceived2)
-                                        .OnDataUpdated(this.OnSubscriptionDataUpdated2)
-                                        .CreateAndStart();
 
-            /*  Task waitEikonDataTaskAsync =  waitEikonDataAsync();
-
-              await waitEikonDataTaskAsync(); */
-
-            // await waitEikonDataAsync();
+            get_data_eikon(par_ricname.ToUpper(), 'e', par_interval[0], par_from_date, par_to_date);
 
             await Task.Run(() =>
             {
@@ -72,53 +81,79 @@ namespace DailyIntervalDemo
                 }
             });
 
-
-            //new Thread(() =>
-            //{
-            //    Thread.CurrentThread.IsBackground = true;
-            //    /* run your code here */
-            //    dataSubscription = DataServices.Instance.TimeSeries.SetupDataRequest(this.Ric)
-            //                    .From(DateTime.Today.AddYears(-5))
-            //                    .WithInterval(CommonInterval.Monthly)
-            //                    .OnDataReceived(OnSubscriptionDataReceived2)
-            //                    .CreateAndSend();
-            //}).Start();
-
-
-            //System.Threading.Thread.Sleep(10000);
-            //  _signal.WaitOne();
-
             return barRecCollection;
-            // lookup person with the requested id 
-            /* return new Person()
-             {
-                 Id = Convert.ToInt32(id),
-                 Name = "Leo Messi"
-             };*/
+            
         }
 
-
-        /*
-    private  Task waitEikonDataAsync()
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "rt_nav_data/{par_ricname}/{par_type}/{par_interval}/{par_from_date}/{par_to_date}")]
+        public async Task<Collection<NavRecord>> getRT_nav_data(string par_ricname, string par_type, string par_interval, string par_from_date, string par_to_date)
         {
+            get_data_eikon(par_ricname.ToUpper(), par_type[0], par_interval[0], par_from_date, par_to_date);
 
-
-            return Task.Factory.StartNew(() =>
+            await Task.Run(() =>
             {
                 while (!dataCompleted)
                 {
                     Thread.Sleep(100);
                 }
-            }
-            );
+            });
 
+            return navRecCollection;
 
-            
-        } */
+        }
 
-    private void OnSubscriptionDataReceived2(DataChunk chunk)
+        private void get_data_eikon(string str_ricname, char chr_type, char chr_interval, string par_from_date, string par_to_date)
+        {
+            dataCompleted = false;
+            barRecCollection.Clear();
+            navRecCollection.Clear();
+
+            string format = "yyyyMMdd";
+            DateTime dt_from = DateTime.ParseExact(par_from_date, format, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime dt_to = DateTime.ParseExact(par_to_date, format, System.Globalization.CultureInfo.InvariantCulture);
+
+            CommonInterval interval = CommonInterval.Daily;
+
+            if (chr_interval == 'd')
+                interval = CommonInterval.Daily;
+            else if (chr_interval == 'w')
+                interval = CommonInterval.Weekly;
+            else if (chr_interval == 'm')
+                interval = CommonInterval.Monthly;
+            else if (chr_interval == 'q')
+                interval = CommonInterval.Quarterly;
+            else if (chr_interval == 'y')
+                interval = CommonInterval.Yearly;
+
+            /* select the view depend on the type of security
+            equity or etf - TRCPRC_1
+            fund - NAV, TRCPRC_1
+            bond - BID, DAILY_RETURN
+            */
+            //default
+            this.view_type = "TRDPRC_1";
+
+            if (chr_type == 'e')
+                this.view_type = "TRDPRC_1";
+            else if (chr_type == 'f')
+                this.view_type = "NAV";
+            else if (chr_type == 'b')
+                this.view_type = "BID";
+
+            dataRequest = DataServices.Instance.TimeSeries.SetupDataRequest(str_ricname)
+                    .From(dt_from).To(dt_to)
+                    .WithView(this.view_type)
+                    .WithInterval(interval)
+                    .OnDataReceived(this.OnRestSubscriptionDataReceived)
+                    .CreateAndSend();
+
+        }
+
+    private void OnRestSubscriptionDataReceived(DataChunk chunk)
     {
-           
+
             // chunk.Records provides access to a dictionary of field -> value, so that
             // developpers can access values from field names and cast them in the proper type like in:
             //                  double open = (double) chunk.Records.First()["OPEN"];
@@ -130,10 +165,22 @@ namespace DailyIntervalDemo
 
             // In this sample we know that records are typical "bar" records (open, close, high, low, volume) 
             // because we ask for a daily interval. So we can use ToBarRecords() helper method:
-            foreach (IBarData record in chunk.Records.ToBarRecords())
+
+            if (this.view_type == "TRDPRC_1") {
+                // BarData from TDRPRC_1 view
+                foreach (IBarData record in chunk.Records.ToBarRecords())
+                {
+                    //Records.Insert(0, record);
+                    barRecCollection.Insert(0, new BarRecord(record));
+                }
+            } else
             {
-                Records.Insert(0, record);
-                barRecCollection.Insert(0, new BarRecord(record));
+                // record from BID or NAV view
+                foreach (IData record in chunk.Records)
+                {
+                    //Records.Insert(0, record);
+                    navRecCollection.Insert(0, new NavRecord(record));
+                }
             }
 
             if (chunk.IsLast)
@@ -142,30 +189,34 @@ namespace DailyIntervalDemo
             }
         }
 
-    private void OnSubscriptionDataUpdated2(IDataUpdate update)
-    {
-            // In this sample for clarity we assume that the realtime update contains at most a single point.
-            // Robust real-world apps should iterate on update.Records.
-            IData record = update.Records.FirstOrDefault();
 
-            if (record != null)
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        UriTemplate = "rt_meta_data/{par_ricname}")]
+        public async Task<MetaDataRecord> getRT_meta_data(string par_ricname)
+        {
+            dataCompleted = false;
+
+            metadataRequest = DataServices.Instance.TimeSeries.Metadata.GetCommonData(par_ricname, onRetCommonData );
+
+            await Task.Run(() =>
             {
-                if (update.UpdateType == UpdateType.ExistingPoint && Records.Count > 0)
+                while (!dataCompleted)
                 {
-                    // In this inter-day context, receiving an update of type "ExistingPoint" means that
-                    // the last received record, ie the record of the current day, is to be overriden.
-                    // Here we simply remove / add the record.
-                    Records.RemoveAt(0);
-                    barRecCollection.RemoveAt(0);
-                    dataCompleted = true;
-                  //  _signal.Set();
+                    Thread.Sleep(100);
                 }
+            });
 
-                Records.Insert(0, record.ToBarRecord());
-                barRecCollection.Insert(0, new BarRecord(record.ToBarRecord()));
-            }
+            return metaDataRec;
+
         }
-}
+
+        private void onRetCommonData(CommonMetadata mdata)
+        {
+            metaDataRec = new MetaDataRecord(mdata);
+            dataCompleted = true;
+        }
+    }
 
 
 
@@ -175,6 +226,9 @@ namespace DailyIntervalDemo
     public partial class MainControl : UserControl, INotifyPropertyChanged
     {
         private ITimeSeriesDataSubscription dataSubscription;
+        public static ITimeSeriesDataRequest dataRequest { get; set; }
+        private IMetadataRequest metadataRequest;
+
         private ServiceHost host;
 
         public MainControl()
@@ -187,7 +241,7 @@ namespace DailyIntervalDemo
                 DataServices.Instance.Initialize("DailyIntervalDemo");
                 this.IsLoading = false;
 
-                string baseAddress = "http://" + Environment.MachineName + ":8733/service1";
+                string baseAddress = "http://" + Environment.MachineName + ":8877/eikon";
                 host = new ServiceHost(typeof(Service1), new Uri(baseAddress));
                 host.AddServiceEndpoint(typeof(IService1), new WebHttpBinding(), "").Behaviors.Add(new WebHttpBehavior());
                 host.Open();
@@ -209,12 +263,25 @@ namespace DailyIntervalDemo
             this.HistoricalDataCount = 0;
             this.IsLoading = true;
 
+            GetCommonMeta(this.Ric);
+            //GetCurrency(this.Ric);
+            //GetHolidaysForRIC();
+
+            /*
             this.dataSubscription = DataServices.Instance.TimeSeries.SetupDataSubscription(this.Ric)
                                         .WithInterval(CommonInterval.Daily)
                                         .OnDataReceived(this.OnSubscriptionDataReceived)
                                         .OnDataUpdated(this.OnSubscriptionDataUpdated)
                                         .OnStatusUpdated(status => this.Title = string.Format("Status: {0}         Error: {1}", status.State, status.Error))
                                         .CreateAndStart();
+                                        */
+
+            dataRequest = DataServices.Instance.TimeSeries.SetupDataRequest(this.Ric)
+                               .From(DateTime.Today.AddYears(-1))
+                               .WithInterval(CommonInterval.Daily)
+                               .OnDataReceived(this.OnSubscriptionDataReceived)
+                               .WithView("BID")
+                               .CreateAndSend();
         }
 
         private void OnSubscriptionDataReceived(DataChunk chunk)
@@ -258,7 +325,22 @@ namespace DailyIntervalDemo
                 this.Records.Insert(0, record.ToBarRecord());
             }
         }
-        
+
+        private void GetCommonMeta(string ric)
+        {
+            // For the sake of conciseness both data and error callbacks are specified as lambda expressions.
+            this.metadataRequest = DataServices.Instance.TimeSeries.Metadata.GetCommonData(ric, metaDatas =>
+            {
+                var meta = metaDatas;
+            }, error =>
+            {
+                RequestErrorType errorType = error.Type;
+                string errorMessage = error.Message;
+                MessageBox.Show(errorMessage, errorType.ToString());
+            });
+        }
+
+      
         // Data services consume native resources that must be disposed as soon as possible.
         // So, it's a recommended practice to dispose data request and susbcription
         // in a deterministic way, based on any kind of events indicating end of application.
